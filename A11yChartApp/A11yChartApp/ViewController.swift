041b5ab23522958101c2b283435a3bc038ab5cc6
横山新 <yokoyamaarata@yokoyamaaratanoMacBook-Pro.local>
2021-07-03 12:18:21 +0900

    [add] ignore

---
+++ A11yChartApp/A11yChartApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A11yChartApp
+//
+//  Created by 横山新 on 2021/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
